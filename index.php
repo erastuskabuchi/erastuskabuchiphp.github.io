1ae084f112f07c657c69c6c6584b09c1230b6e49
Your Name <you@example.com>
2018-03-13 13:52:52 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>my index</title>
+	<link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.css">
+</head>
+<body>
+<nav class="navbar navbar-inverse">
+    	<div class="container fluid">
+    		<div class="navbar-header">
+    			<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#mynavbar">
+    			<span class="icon-bar"></span>
+    			<span class="icon-bar"></span>
+    			<span class="icon-bar"></span>
+    			</button>
+    			<a class="navbar-brand" href="#">My work</a>
+    		</div>
+    		<div class="collapse navbar-collapse" id="my navbar">
+    			<ul class="nav navbar-nav navbar-right">
+    				<li class="active"><a href="#">Home</a></li>
+    				<li><a href="#">About</a></li>
+    				<li><a href="#">Skills</a></li>
+    				<li><a href="#">Contacts</a></li>
+    			</ul>
+    		</div>
+    	</div>	
+    	</nav>
+	<div class="container text-center">
+		<div class="jumbotron">
+			<div><h1 style="color: pink;">WELCOME TO MY WEB</h1></div>
+			<div><h1><strong><u>login from here</u></strong></h1></div>
+			<a href="login.php"><button class="btn btn-success"> login</button></a>
+		</div>
+	</div>
+
+</body>
+</html>
